fa6b3ea8c4b3d98da885e1d53242b6804fa58d21
Giuseppe11S <senagiuseppe11@gmail.com>
2025-09-19 12:12:00 +0200

    Created an overlay of each img in cards

--- script.js
+++ script.js
@@ -1,32 +1,63 @@
 const callApi = 'https://lanciweb.github.io/demo/api/pictures/';
 const backgroundCard = document.querySelector('.background-card');
+const overlay = document.querySelector('.overlay');
+const overlayImage = document.querySelector('.overlay-image');
+const closeBtn = document.querySelector('.close-btn');
 let pictureGen = [];
 
+
+
+// richiesta ad l'api
 function getPicApi() {
   axios.get(callApi)
-    .then(response => {
+    .then(function(response) {
       pictureGen = response.data;
-      console.log(pictureGen);
       renderCards();
-    })
-};
+    });
+}
 
 function renderCards() {
+
+  // variabile dove andranno le cards
   let cardsHTML = '';
-  
-  pictureGen.forEach((objData) => {
+
+  pictureGen.forEach(function(objData) {
     cardsHTML += `
       <div class="card">
         <img class="pin-picture" src="img/pin.svg" alt="Pin">
-        <img src="${objData.url}" alt="${objData.title}">
+        <img class="card-image" src="${objData.url}" alt="${objData.title}">
         <h2>${objData.title}</h2>
         <p>${objData.date}</p>
       </div>
     `;
   });
-  
+
   backgroundCard.innerHTML = cardsHTML;
+  
+  const cardImages = document.querySelectorAll('.card-image');
+
+   // collego le img generate all'overlay
+   
+  cardImages.forEach(function(image) {
+    image.addEventListener('click', () => {
+
+      overlayImage.src = image.src;
+      overlayImage.alt = image.alt;
+      overlay.classList.remove('overlay-ds-none');
+
+    });
+  });
 }
+
+
+// chiusura dell'overlay tramite button
+closeBtn.addEventListener('click', () => {
+
+  overlay.classList.add('overlay-ds-none');
+  overlayImage.src = '';
+  overlayImage.alt = '';
+
+});
 
 
 getPicApi();
